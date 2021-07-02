004e638ee842323b7994dc43ee161bce6e3d72b0
Svante Bengtson <svante@swantzter.se>
2021-07-02 20:44:12 +0000

    pretty print sitemap

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -19,12 +19,12 @@ export const sitemapGet = functions.https.onRequest(async (req, res) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${qSnap.docs.map(dSnap => `  <url>
+${qSnap.docs.map(dSnap => `  <url>
     <loc>https://the-tricktionary.com/trick/${disciplineToSlug(dSnap.get('discipline'))}/${dSnap.get('slug')}</loc>
     <lastmod>${dSnap.updateTime.toDate().toString()}</lastmod>
     <changefreq>yearly</changefreq>
   </url>`).join('\n')}
 </urlset>`
 
-  res.send(sitemap)
+  res.status(200).send(sitemap)
 })
