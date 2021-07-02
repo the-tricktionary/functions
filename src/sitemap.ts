b66acf8355730e5e7dd3dc4d4b35bd2362924237
Svante Bengtson <svante@swantzter.se>
2021-07-02 20:55:49 +0000

    remove lastmod

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -18,13 +18,14 @@ export const sitemapGet = functions.https.onRequest(async (req, res) => {
   const qSnap = await firestore().collection('tricks').get()
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${qSnap.docs.map(dSnap => `  <url>
     <loc>https://the-tricktionary.com/trick/${disciplineToSlug(dSnap.get('discipline'))}/${dSnap.get('slug')}</loc>
-    <lastmod>${dSnap.updateTime.toDate().toISOString()}</lastmod>
     <changefreq>yearly</changefreq>
   </url>`).join('\n')}
-</urlset>`
+</urlset>
+`
 
   res.status(200).send(sitemap)
 })
